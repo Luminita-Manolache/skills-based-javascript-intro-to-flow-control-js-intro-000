6314a0b82a0c8a5ca336f5e24ac7434a13ab4c6f
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-01-26 08:02:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age <= 12) {
-    console.log("You are a kid");
+    return "You are a kid");
   } else if (age > 13 && age < 19) {
     return "You are a teenager!";
   } else {
