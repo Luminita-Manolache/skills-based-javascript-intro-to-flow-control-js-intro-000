d7d0a2c0a7b77ccc68a6d015aca847417e253f34
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-01-26 07:28:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,11 @@
 function basicTeenager(age) {
-   if(age > 13 && age < 19) {
+   if (age > 13 && age < 19) {
      return "You are a teenager!";
    }
 }
 
 function teenager(age) {
-  if(age > 13 && age < 19) {
+  if (age > 13 && age < 19) {
     return "You are a teenager!";
   } else {
     return "You are not a teenager";
@@ -13,9 +13,9 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if(age > 13 && age < 19) {
+  if (age > 13 && age < 19) {
     return "You are a teenager!";
-  } else if(age <= 12) {
+  } else if (age <= 12) {
     return "You are a kid";
   } else {
     return "You are a grownup";
